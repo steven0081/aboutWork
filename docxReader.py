dee6b9a36c667a8224a062e472c17b648dc1263d
joe <405886056@qq.com>
2019-07-18 17:00:16 +0800

    modify

--- docxReader.py
+++ docxReader.py
@@ -1,18 +1,20 @@
-import docx , os
-import openpyxl
+import docx, os
 from docx import Document
 
-file_name =  os.getcwd()+'\\' + '附件12.2019年省级工业互联网发展专项资金申报指南.docx'
+file_name =  os.getcwd()+'\\' + 'A1-华新水泥大数据平台-ok.docx'
 doc = docx.Document(file_name)
 paragraphs = doc.paragraphs
 print(len(paragraphs))
 print(paragraphs[4].text)
 document = Document(file_name)
 tables = document.tables
+print('#################################处理表格#############################')
 print(len(tables))
-table = tables[0]
-for i in range(0,len(table.rows)):
-    row_cells = table.rows[i].cells
-    for cell in row_cells:
-        print(cell.text)
+#table = tables[0]
+for table in tables:
+    for i in range(0, len(table.rows)):
+        print(i)
+        row_cells = table.rows[i].cells
+        for cell in row_cells:
+            print(cell)
 
